88d40f0220a3e282bd64554e530c7505523b6e64
Ilya Reshetnikov <devxom@gmail.com>
2016-09-05 23:02:15 +0300

    [#0006] Ref translate-selected-[ru].js
    More details
    
    * Simplify code
    * Refactor

--- translate-selected-[ru].js
+++ translate-selected-[ru].js
@@ -1,8 +1,5 @@
 // Bookmarklets
-// javascript:var%20a%20=%20window;var%20o%20=%20window.getSelection();var%20s%20=%20o.getRangeAt(0);a.open("https://translate.google.com/#auto/ru/"%20+%20s,%20'_blank');
+// window.open("https://translate.google.com/#auto/ru/"+window.getSelection().getRangeAt(0),"_blank");
 
 // Javascript
-var a = window;
-var o = window.getSelection();
-var s = o.getRangeAt(0);
-a.open("https://translate.google.com/#auto/ru/" + s, '_blank');
+window.open("https://translate.google.com/#auto/ru/"+window.getSelection().getRangeAt(0),"_blank");
