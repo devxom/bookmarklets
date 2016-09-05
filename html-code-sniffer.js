a379533a587139e3310dea45f0cd8ec2fc355ffb
Ilya Reshetnikov <devxom@gmail.com>
2016-09-05 23:17:51 +0300

    [#0010] Add html-code-sniffer.js

---
+++ html-code-sniffer.js
@@ -0,0 +1,23 @@
+// javascript:(function()%20{var%20_p='//squizlabs.github.io/HTML_CodeSniffer/build/';var%20_i=function(s,cb)%20{var%20sc=document.createElement('script');sc.onload%20=%20function()%20{sc.onload%20=%20null;sc.onreadystatechange%20=%20null;cb.call(this);};sc.onreadystatechange%20=%20function(){if(/^(complete|loaded)$/.test(this.readyState)%20===%20true){sc.onreadystatechange%20=%20null;sc.onload();}};sc.src=s;if%20(document.head)%20{document.head.appendChild(sc);}%20else%20{document.getElementsByTagName('head')[0].appendChild(sc);}};%20var%20options={path:_p};_i(_p+'HTMLCS.js',function(){HTMLCSAuditor.run('WCAG2AA',null,options);});})();
+
+// Javascript
+
+(
+    function() {
+        var _p = '//squizlabs.github.io/HTML_CodeSniffer/build/';
+        var _i = function(s, cb) {
+            var sc = document.createElement('script');
+            sc.onload = function() { sc.onload = null;
+                sc.onreadystatechange = null;
+                cb.call(this); };
+            sc.onreadystatechange = function() {
+                if (/^(complete|loaded)$/.test(this.readyState) === true) { sc.onreadystatechange = null;
+                    sc.onload(); } };
+            sc.src = s;
+            if (document.head) { document.head.appendChild(sc); } else { document.getElementsByTagName('head')[0].appendChild(sc); } };
+        var options = { path: _p };
+        _i(_p + 'HTMLCS.js', function() { HTMLCSAuditor.run('WCAG2AA', null, options); });
+    }
+)();
+
+// squizlabs.github.io/HTML_CodeSniffer/build/HTMLCS.js
