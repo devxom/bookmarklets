5a946a66f8c195b68f2c36a36346618ebda3adf5
Ilya Reshetnikov <devxom@gmail.com>
2016-09-05 21:56:32 +0300

    upd:translating bookmarklets

--- translate-selected-[ru].js
+++ translate-selected-[ru].js
@@ -1,7 +1,8 @@
 // Bookmarklets
-// javascript:var%20selObj%20=%20window.getSelection();%20var%20selRange%20=%20selObj.getRangeAt(0);%20window.open("https://translate.google.com/#auto/ru/"%20+%20selRange%20+%20"",%20'_blank');
+// javascript:var%20a%20=%20window;var%20o%20=%20window.getSelection();var%20s%20=%20o.getRangeAt(0);a.open("https://translate.google.com/#auto/ru/"%20+%20s,%20'_blank');
 
 // Javascript
-var selObj = window.getSelection();
-var selRange = selObj.getRangeAt(0);
-window.open("https://translate.google.com/#auto/ru/" + selRange + "", '_blank');
+var a = window;
+var o = window.getSelection();
+var s = o.getRangeAt(0);
+a.open("https://translate.google.com/#auto/ru/" + s, '_blank');
