25583fd3b2a2da2fa764dd59b82283cff3b3a1b7
Ilya Reshetnikov <devxom@gmail.com>
2016-09-05 23:15:39 +0300

    [#0008] Add ukeeper bookmarklet
    * Add ukeeper.js bookmarklet for send to ukeeper.com

---
+++ ukeeper.js
@@ -0,0 +1,26 @@
+// get from ukeeper.com
+
+// javascript:(function(){var%20a=window,b=document,c=encodeURIComponent,i=b.createElement('iframe'),s=i.style;i.src='https://ukeeper.com/action/grab?id=889130413619a19fb5169522858163711bf18f3c&url='+c(b.location)+'&email=devxom@gmail.com';s.background='#000';s.color='#fff';s.position='absolute';s.top='18px';s.left='18px';s.width%20='320px';s.height='160px';s.border='0%20none';s.borderRadius='6px';s.boxShadow='0%200%208px%20rgba(0,0,0,.8)';s.opacity='.9';s.zIndex='99999';b.body.appendChild(i);a.setTimeout(function(){i.style.display='none';delete%20i},1500)})();
+
+javascript: (
+        function() {
+            var % 20 a = window, b = document, c = encodeURIComponent, i = b.createElement('iframe'), s = i.style;
+            i.src = 'https://ukeeper.com/action/grab?id=889130413619a19fb5169522858163711bf18f3c&url=' + c(b.location) + '&email=devxom@gmail.com';
+            s.background = '#000';
+            s.color = '#fff';
+            s.position = 'absolute';
+            s.top = '18px';
+            s.left = '18px';
+            s.width % 20 = '320px';
+            s.height = '160px';
+            s.border = '0%20none';
+            s.borderRadius = '6px';
+            s.boxShadow = '0%200%208px%20rgba(0,0,0,.8)';
+            s.opacity = '.9';
+            s.zIndex = '99999';
+            b.body.appendChild(i);
+            a.setTimeout(function() {
+                i.style.display = 'none';
+                delete % 20 i
+            }, 1500)
+        }
